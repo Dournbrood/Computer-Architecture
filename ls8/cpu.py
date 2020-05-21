250c65e820e9b156fa7273c0511ab84e04ed99a8
Dournbrood <tanner1220002@gmail.com>
2020-05-21 16:13:05 -0400

    day 4 MVP completion

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,7 +12,7 @@ class CPU:
         self.ram = [0] * 256
         self.pc = 0
         self.instructions = {"HLT": 0b00000001,
-                             "LDI": 0b10000010, "PRN": 0b01000111, "MLT": 0b10100010, "PSH": 0b01000101, "POP": 0b01000110}
+                             "LDI": 0b10000010, "PRN": 0b01000111, "MLT": 0b10100010, "PSH": 0b01000101, "POP": 0b01000110, "CLL": 0b01010000, "RET": 0b00010001, "ADD": 0b10100000}
         self.halted = False
         self.sp = 7
 
@@ -74,57 +74,64 @@ class CPU:
 
         print()
 
+    def push(self):
+
+        # Decrement the stack pointer by 1
+        self.reg[self.sp] -= 1
+
+        # The registry at `self.sp` points to the address in our `self.ram` that is the top of the stack. It defaults to 0xf4.
+        stack_top_addr = self.reg[self.sp]
+
+        # Change the value in the address that is the top of our stack to whatever value we grabbed from the registry earlier.
+        self.ram[stack_top_addr] = self.pc
+
+    def pop(self):
+
+        # The registry at `self.sp` points to the address in our `self.ram` that is the top of the stack. It defaults to 0xf4.
+        stack_top_addr = self.reg[self.sp]
+
+        # Change the value at the specified registry to the value at the top of the stack.
+        return self.ram[stack_top_addr]
+
     def run(self):
         while not self.halted:
 
+            # self.trace()
+
+            # print(f"self.pc: {self.pc}")
+
             # print(self.reg)
             # print(self.ram)
+            # print(self.ram[self.pc])
 
             if self.ram[self.pc] == self.instructions["HLT"]:
+                # print("HLT")
                 self.halted = True
                 break
 
-            if self.ram[self.pc] == self.instructions["LDI"]:
+            elif self.ram[self.pc] == self.instructions["LDI"]:
+                # print("LDI")
                 self.pc += 1
                 self.reg[self.ram[self.pc]] = self.ram[self.pc + 1]
                 self.pc += 2
 
-            if self.ram[self.pc] == self.instructions["PRN"]:
+            elif self.ram[self.pc] == self.instructions["PRN"]:
+                # print("PRN")
                 self.pc += 1
                 print(f"{self.reg[self.ram[self.pc]]}")
                 self.pc += 1
 
-            if self.ram[self.pc] == self.instructions["MLT"]:
+            elif self.ram[self.pc] == self.instructions["MLT"]:
+                # print("MLT")
                 self.pc += 1
                 self.alu("MLT", self.ram[self.pc],
                          self.ram[self.pc + 1])
                 self.pc += 2
 
-            if self.ram[self.pc] == self.instructions["PSH"]:
-                """ 
-                Stack
-                ----------
-                5 <---
-                4
-                3
-                2
-                1
-                ----------    
-                """
+            elif self.ram[self.pc] == self.instructions["PSH"]:
+                # print("PSH")
                 # Decrement the stack pointer by 1
                 self.reg[self.sp] -= 1
-
-                """ 
-                Stack
-                ----------
-                ? <---
-                5 
-                4
-                3
-                2
-                1
-                ----------    
-                """
 
                 # Get register number
                 reg_num = self.ram[self.pc + 1]
@@ -140,8 +147,8 @@ class CPU:
 
                 self.pc += 2
 
-            if self.ram[self.pc] == self.instructions["POP"]:
-
+            elif self.ram[self.pc] == self.instructions["POP"]:
+                # print("POP")
                 # When popping to registry 0, set the registry[0] value to the value at the top of the stack, and then remove it from the stack.
 
                 # Get register number
@@ -156,34 +163,41 @@ class CPU:
                 # Change the value in the address that is the top of our stack to 0.
                 self.ram[stack_top_addr] = 0
 
-                """ 
-                Stack
-                ----------
-                ...
-                6 <---
-                5 
-                4
-                3
-                2
-                1
-                ...
-                ----------    
-                """
                 # Increment the stack pointer by 1
                 self.reg[self.sp] += 1
 
-                """ 
-                Stack
-                ----------
-                ...
-                0 
-                5 <---
-                4
-                3
-                2
-                1
-                ...
-                ----------    
-                """
+                self.pc += 2
+
+            elif self.ram[self.pc] == self.instructions["ADD"]:
+                # print("ADD")
+                self.pc += 1
+                self.alu("ADD", self.ram[self.pc], self.ram[self.pc + 1])
 
                 self.pc += 2
+
+            elif self.ram[self.pc] == self.instructions["CLL"]:
+                # print("CLL")
+                # Put our current `pc` or address in memory in the stack.
+                self.push()
+
+                # self.trace()
+
+                # Change `pc` to whatever address the `CALL` wants us to.
+                self.pc = self.reg[self.ram[self.pc + 1]]
+                # print(f"self.pc: {self.pc}")
+
+                # print(f"value at self.ram[self.pc]: {self.ram[self.pc]}")
+
+            elif self.ram[self.pc] == self.instructions["RET"]:
+                # print("RET")
+                self.pc = self.pop()
+
+                self.pc += 2
+
+                # Change the value in the address that is the top of our stack to 0.
+                stack_top_addr = self.reg[self.sp]
+
+                self.ram[stack_top_addr] = 0
+
+                # Increment the stack pointer by 1
+                self.reg[self.sp] += 1
